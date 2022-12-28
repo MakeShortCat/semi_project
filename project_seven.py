a1e8f0488c48df376824321b5bf05e5c70f09562
MakeShortCat <pgs6630@gmail.com>
2022-12-29 00:35:53 +0900

    기초 설계

---
+++ project_seven.py
@@ -0,0 +1,10 @@
+#웹서버 설치 : streamlit
+#가상환경 설치 : anaconda
+#conda create -n prj python=3.7.4 ipython numpy matplotlib pandas scipy
+#ls : 파일 혹은 디렉토리 이름 확인 명령어
+#cd : 디렉토리 변경 명령어
+#상위 디텍고리로 : cd..
+#pwd : 현재 폴더 확인 하는 명령어
+#exit() : 빠져나오기
+#streamlit run app.py
+
