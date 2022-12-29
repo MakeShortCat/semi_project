8b5953c6d75d41b3bbc0e155f71ff3271a281b96
MakeShortCat <pgs6630@gmail.com>
2022-12-29 17:58:10 +0900

    세미 프로젝트 관련 입구 페이지 정보

--- pages/project1.py
+++ pages/project1.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from PIL import Image
-from utils import project1_desc as p1d
-
+from utils import project1_program as p1p
+st.set_page_config(layout="wide")
 def app():
-	st.write('''
-		### 세미 프로젝트
-		'''
-		)
-
-	p1d.desc()
+    st.write('''##세미프로젝트
+             ''')
+    
+    Names = ['개발 환경 구축 정보 및 매뉴얼', 'Streamlit을 활용한 웹서버 구축 및 Diagram', 
+             'Streamlit 매뉴얼', '자료 수집 과정', '자료 처리 Diagram']
+    
+    p1p.program_manual(Names[0])
+    p1p.Streamlit_Diagram(Names[1])
